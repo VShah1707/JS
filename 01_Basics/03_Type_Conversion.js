c3957cc553ed3367f3d89869627a17e8885c7262
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-12 17:28:26 +0000

    Operations

--- 01_Basics/03_Type_Conversion.js
+++ 01_Basics/03_Type_Conversion.js
@@ -1,20 +1,48 @@
-let stringValue = "123"
+// let stringValue = "123"
 // console.log(Number(stringValue))  will give NaN but the type will be number
-console.log(Number(null), Number(undefined), Number(true))
+
+// console.log(Number(null), Number(undefined), Number(true))
+
 //  Typeof all the above consoles will be number the the values will be different
 
 // ================================= Boolean
-let boolean = 0
+// let boolean = 0
 
-console.log(Boolean(boolean), typeof Boolean(boolean))
-console.log(Boolean(stringValue), typeof Boolean(stringValue))
+// console.log(Boolean(boolean), typeof Boolean(boolean))
+// console.log(Boolean(stringValue), typeof Boolean(stringValue))
 
 // 0/1 => boolead => false/true
 //  "" => boolean => false
 // "value" => boolean => true
 
-let number = 123
-let booleanValue = true
-console.log(String(123), typeof String(number))
-console.log(String(booleanValue), typeof String(booleanValue))
+// let number = 123
+// let booleanValue = true
+// console.log(String(123), typeof String(number))
+// console.log(String(booleanValue), typeof String(booleanValue))
 
+// ===================== Operation =====================
+
+let value = 3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// console.log(1 + '2' + 2)
+// console.log(1 ** 2 )
+// console.log(1 + 2 * 8 / 2 % 2 )
+
+
+// console.log(true + true);
+// console.log(typeof +'abc') number 
+
+let a = 1
+let  b = a++
+let  c = ++a
+console.log(a,b,c,a)
+
+// ================
