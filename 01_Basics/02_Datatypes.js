3851e8959bf4afb1aee70d3658769c9089f5923d
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-12 16:43:01 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS as newer version
+// string
+// number
+// boolean
+// undefined
+// null
+// bigInt
+// symbol
+
+// object ===> non-primitive
+// typeof is used to get the type of any data type
