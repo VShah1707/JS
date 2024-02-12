013d5a11af03d0b2e801240db7e42577cb1ad0d5
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-12 16:45:06 +0000

    Type conversion Number, boolean & string

---
+++ 01_Basics/03_Type_Conversion.js
@@ -0,0 +1,20 @@
+let stringValue = "123"
+// console.log(Number(stringValue))  will give NaN but the type will be number
+console.log(Number(null), Number(undefined), Number(true))
+//  Typeof all the above consoles will be number the the values will be different
+
+// ================================= Boolean
+let boolean = 0
+
+console.log(Boolean(boolean), typeof Boolean(boolean))
+console.log(Boolean(stringValue), typeof Boolean(stringValue))
+
+// 0/1 => boolead => false/true
+//  "" => boolean => false
+// "value" => boolean => true
+
+let number = 123
+let booleanValue = true
+console.log(String(123), typeof String(number))
+console.log(String(booleanValue), typeof String(booleanValue))
+
