a05f8740f67f790ec09bf23631663e5630c1c44e
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-12 16:42:51 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+const name = 'varshil'
+let email = 'test.test@example.com'
+var accPassword = "123456"
+cityName = 'ahmedavad'
+let accState
+
+
+// const can not be modified
+// let can be modified
+
+console.table([name,email,accPassword, cityName,accState])
