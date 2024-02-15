6bce7169b55c7d8371f9775de45afbce776a9aee
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-15 06:16:01 +0000

    Datatype Summary

--- 01_Basics/04_Comparison.js
+++ 01_Basics/04_Comparison.js
@@ -2,4 +2,12 @@
 console.log('1' === 1);
 console.log('1' == 1);
 
+console.log(null >= 0)
+console.log(null == 0)
+// equality check and comparison check works differently. Comparison converts the null to 0 
+//  therefore >= is true in this case
+// Specifically avoid such comparisons
+
+console.log("2" == 2); // Will give true as it does type conversion
+console.log("2" === 2); // Will give false as it check strickly
 
---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,15 @@
+// Primitive Datatypes 
+
+// String
+// Number
+// Boolean
+// Undefined
+// Null
+// BigInt   
+// Symbol   ===> Used to make unique values
+
+// Non-Primitive (Reference type)
+
+// Objects, Array, Function (Their Types are objects)
+
+// typeOf function is Function but actualy it is called as object Fuction
