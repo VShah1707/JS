0a45b57da31e2276c154d795413c68d6fba79c00
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-15 11:28:25 +0000

    String and its method

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,30 @@
+// string.at() & string.charAt() => takes number and returns particular letter at that index
+//  string.charCodeAt() => takes number and return character code of the letter
+// string.concat() => takes two strings and concat them string.concat(str1,str2)
+// string.endsWith() => returns boolean value on the basis of whether the string ends with particular value string.endsWith("hello")
+// string.includes() => check whether the provided string is present or not and returns boolean value
+// string.indexOf() => search and returns the first occurance of the substring
+// string.padEnd(targetLength,padString)  => helps to pad string at the end of the string 
+// string.padStart(targetLength,padString) => helps to pad string at the start of the string
+// string.repeat() => construct and returns a new string wich contains specified number of copies of that string
+// string.replace("value to be change", "new value")  => returns new string the replacement of older value with newer works with first occurance
+// string.replaceAll(pattern, replacement) => replace all the pattern with replacement
+//  string.search() => to search specific regex or sting
+
+// string.slice(startIndex,endIndex) || string.slice(startIndex) => return new string after or between the range of startIndex and endIndex
+// string.split(separator) => split the string using the separator and returns array
+// string.startsWith() => returns boolean
+// string.subString(start,end) => return sub string on the basis of the start and the end
+// string.toLocaleLowerCase() => values returns this string converted to lower case, according to any locale-specific case mappings.
+// string.toLocaleUpperCase() => values returns this string converted to upper case, according to any locale-specific case mappings.
+// string.toLowerCase() => values this string converted to lower case
+// string.toUpperCase() => values this string converted to upper case
+// string.toString() => return string value
+// string.trim() => trim the white spaces from the string (.trimStart() || .trimEnd())
+
+
+
+const sentence = "Hi ! my name is varshil"
+// console.log(sentence.slice(4,15))
+// console.log(sentence.split("!"))
+console.log(demoLocal.toLocaleLowerCase())
