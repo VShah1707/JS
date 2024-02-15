98c989427947040fd8b59e71f7b22aef3ca5d0aa
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-15 13:12:00 +0000

    Number and Math

---
+++ 01_Basics/06Number_and_math.js
@@ -0,0 +1,33 @@
+const score = new Number(150)
+const balance = 450
+// console.log(score);
+// console.log(balance);
+
+// toString()
+// console.log(score.toString());
+
+// toFixed()
+// console.log(score.toFixed(2));
+
+const newNumber = 23.4568
+
+
+// toPrecision()
+// console.log(newNumber.toPrecision(3));  
+
+// toLocaleString()
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// =================================Math==================================
+// Math.abs => converts negative to positive
+// console.log(Math.round(4.69))
+// console.log(Math.ceil(4.75));
+// console.log(Math.floor(4.75));
+const max = 10
+const min = 20
+console.log((Math.floor(Math.random() * (max-min+1)) + min ));
+
+
+    
