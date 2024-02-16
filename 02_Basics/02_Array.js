2c3e26a928bc34b32a0fc04a9e97eb9a2f7a739b
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-16 12:37:43 +0000

    Array-02

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,10 @@
+const vowels = ['a','e','i','o','u']
+const numbers = [1,5,6,3,9]
+
+// concat : return new array
+// spread Operator (...) 
+// flat : used to flatten all the nested arrays and returns a new array
+// Array.its-methods ==> check it 
+
+// console.log(vowels.concat(numbers));
+
