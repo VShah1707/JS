4ea85e5ce936c911fd62f88c5fb8182aa2d431af
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-16 10:55:42 +0000

    Array methods-1

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,16 @@
+const myArray = [1,5,3,6,9,7]
+
+// Array Methods
+// push
+// pop
+// shift : remove first element
+// unShift : add any element at first position
+// includes : returns boolean
+// indexOf 
+// join : retun all the elements in string format
+// slice : will return a copy of section of array.
+// splice : will make changes in the existing array
+
+// console.log(myArray.join());
+const newArr = myArray.slice(1,4)
+// const newArrSPlice = myArray.splice(1,4)
