640569d799d517de6fd6a1894ba21791fc26e1ea
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-16 06:39:22 +0000

    Date and Time

---
+++ 01_Basics/07_Date_And_Time.js
@@ -0,0 +1,13 @@
+const myDate = new Date()
+// For only Date
+// console.log(myDate.toDateString());
+// Get UTC time
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toJSON());
+// const createdDate = new Date()
+// console.log(createdDate.toLocaleString());
+// console.log(Math.floor(createdDate.getTime()/1000));
+
+console.log(myDate.getMonth());
+console.log(myDate.getDay());
