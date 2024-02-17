e1364fa232dc3f32d636f818d9501d41194d7f5a
varshil shah <varshil.shah@ecosmob.com>
2024-02-18 01:05:24 +0530

    Object-destructuring

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -34,3 +34,17 @@ const obj3 = {
 // const result = Object.assign({},obj1, obj2, obj3)
 // console.log(result);
 // console.log(obj1);
+
+
+// ===================== Object Destructuring ================================
+// We can san unpacking of values of any object or array
+
+// const myArray = [1,2,3,4,5,6]
+// const [a,b,c,...rest] = myArray
+
+const course = {
+    courseName : "JS",
+    price : 999,
+    teacher : 'Hitesh'
+}
+const {courseName : name, teacher} = course
