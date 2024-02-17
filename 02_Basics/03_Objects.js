a9dc90a2a19a1e534b77a17f4e3a591bcb713743
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-17 14:55:13 +0000

    Objects - 01

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,19 @@
+// Object Literals
+const mySYmbol = Symbol('key')
+const myObj = {
+    name: 'Varshil',
+    age: 22,
+    location: 'Ahmedavad',
+    email: 'varshil@gmail.com',
+    isLoggedIn: false,
+    [mySYmbol]: 'This is How Symbol is Refered',
+}
+// console.log(myObj.age);
+// console.log(myObj[mySYmbol]);
+// Object.freeze(myObj)  : it will freeze the object preventing it for further modication
+
+myObj.greeting = function(){
+    console.log("Hello World");
+}
+
+console.log(myObj.greeting())
