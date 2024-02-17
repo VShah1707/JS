5fc96cbcaead8613bfc2252373a138f2ddd7550e
varshil shah <varshil.shah@ecosmob.com>
2024-02-17 22:43:51 +0530

    Object-2

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -1,9 +1,12 @@
- // Singleton or with the help of constructor
+// Singleton or with the help of constructor
 // const tinder = new Object()
 // tinder.id = 123
 // tinder.name = 'Varshil'
 // tinder.isLoggedIn = true
 // console.log(tinder);
+
+// Object.assign(target,source) : return new object 
+
 
 const regularUser = {
     email : 'test@test.com',
@@ -20,7 +23,14 @@ const obj2 = {
     3 : 'c',
     4 : 'd'
 }
-
-const obj3 = {obj1,obj2}
-
-console.log(obj3);
+const obj3 = {
+    3 : 'c',
+    4 : 'd'
+}
+// obj1.hasOwnProperty() ==> to check whether the key is present or not
+// Object.entries() ==> return an array of ['key' , "value"] 
+// console.log(Object.keys(obj1));  To get the keys or values as well
+// const result = {...obj1, ...obj2, ...obj3}
+// const result = Object.assign({},obj1, obj2, obj3)
+// console.log(result);
+// console.log(obj1);
