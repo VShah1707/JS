693442f30033fda683fbc035fa628cb22c6d550b
varshil shah <varshil.shah@ecosmob.com>
2024-02-18 17:04:52 +0530

    Function_02

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -14,12 +14,32 @@ addNumbers(1,2)
 const result = addNumbers(5,9)
 // console.log(result);
 
-function userLoginMessage(userName = "User") {   // Default value
-     if(userName){
-          return `${userName} just logged in`
-     }
-     console.log("Please Enter Username");
-}
+// function userLoginMessage(userName = "User") {   // Default value
+//      if(userName){
+//           return `${userName} just logged in`
+//      }
+//      console.log("Please Enter Username");
+// }
 
 // console.log(userLoginMessage("Varshil"))
-console.log(userLoginMessage())
+// console.log(userLoginMessage())
+
+function calculatePrice(...args){
+     return args
+}
+
+// const arguments = calculatePrice(200,400,500,800)
+// console.log(arguments);
+
+const userObject = {
+     userName : "Varshil",
+     age : 22,
+     profession : 'Developer'
+}
+
+function handleObject({userName,age,profession}){
+     console.log(`Username is ${userName}. His age is ${age} & he is a ${profession} `);
+}
+
+handleObject(userObject)
+
