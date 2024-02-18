796d2878ede60c0beeae7b3146c4223f864c85dc
varshil shah <varshil.shah@ecosmob.com>
2024-02-18 21:34:42 +0530

    IIFE

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function code() {
+    console.log("Hello World");
+})();
+// ; is mandatory to complete its execution 
+// ()() : first one is the function definition & second one is its execution
+// Used to deal with the pollution of global scope
+// (() => {})()   :  this is IIFE is invoked using arrow function
