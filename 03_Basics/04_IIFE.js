6872573be2856f827e8d1256077da2ef5b6520e2
varshil shah <varshil.shah@ecosmob.com>
2024-02-18 21:34:54 +0530

    IIFE

--- 03_Basics/04_IIFE.js
+++ 03_Basics/04_IIFE.js
@@ -6,4 +6,4 @@
 // ; is mandatory to complete its execution 
 // ()() : first one is the function definition & second one is its execution
 // Used to deal with the pollution of global scope
-// (() => {})()   :  this is IIFE is invoked using arrow function
+// (() => {})()   :  this is IIFE is invoked using arrow function 
