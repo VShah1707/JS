16f79073fa3e16e579075f25b4bc8385c6e9c772
VShah1707 <99525067+VShah1707@users.noreply.github.com>
2024-02-23 10:37:16 +0000

    Do While

---
+++ 05_Iterations/02_while_do-while.js
@@ -0,0 +1,15 @@
+// while
+
+// let index = 0
+// while(index < 10){
+//     console.log(index);
+//     index = index + 2
+// }
+
+// do while
+
+let index = 100
+do{
+    console.log(index);
+    index ++
+}while(index < 15)
