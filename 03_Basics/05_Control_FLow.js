f90f644f96f2683efc16eb6d8e7a0ac763ad8448
varshil shah <varshil.shah@ecosmob.com>
2024-02-24 15:09:09 +0530

    IIFE & Control Flow

---
+++ 03_Basics/05_Control_FLow.js
@@ -0,0 +1,5 @@
+// if else
+// switch 
+// truthee & falsee values
+// falsee => false, 0, "", -0, bigInt, null, undefined, NaN
+// except these are truthee values : "0", "false", [], {}, function(){}," "
