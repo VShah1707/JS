c584ead2bbd8bc4c3d5f9a89fa9b16cd4cb1918c
varshil shah <varshil.shah@ecosmob.com>
2024-03-17 17:49:31 +0530

    theme changer project complete

--- 07_projects/Theme_Changer/theme_changer.js
+++ 07_projects/Theme_Changer/theme_changer.js
@@ -1,3 +1,23 @@
-const bodyElement = document.querySelector('body')
-const buttons = document.querySelectorAll('.box')
-console.log(buttons)
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target)
+    if(e.target.id === 'grey'){
+        body.style.background = 'grey'
+    }
+    if(e.target.id === 'white'){
+        body.style.background = 'white'
+    }
+    if(e.target.id === 'blue'){
+        body.style.background = 'blue'
+    }
+    if(e.target.id === 'yellow'){
+        body.style.background = 'yellow'
+    }
+  });
+});
+
